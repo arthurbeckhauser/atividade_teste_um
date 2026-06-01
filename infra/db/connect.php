2d3ccd68666aec49d4733b51eeba53c66179191c
arthurbeckhauser <arthur_b_fernandes@estudante.sesisenai.org.br>
2026-06-01 12:37:03 -0300

    add - connect

---
+++ infra/db/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $db = "sistema_simples_m1";
+
+    $conn = new mysqli($host,$user,$pass,$db);
+
+//Esse código faz uma conexão entre a aplicação PHP e o banco de dados MySQL, e verifica se a conexão foi feita com sucesso ou se teve algum erro.
+
+    if($conn->connect_error){
+        die("Erro na conexão!");
+    }else{
+        echo "<script>console.log('Banco conectado com sucesso!')</script>";
+    };
+
+?>
