2b1181ae2ab887c669d329e7ba1976142cf72800
arthurbeckhauser <arthur_b_fernandes@estudante.sesisenai.org.br>
2026-06-01 12:41:01 -0300

    add - connect

---
+++ public/components/table.php
@@ -0,0 +1,34 @@
+<h4>Usuários Cadastrados</h4>
+
+<table border="1" cellpadding="3">
+
+    <tr>
+        <th>ID</th>
+        <th>Usuário</th>
+        <th>Senha</th>
+    </tr>
+
+    <?php
+    
+    $sqlTodosUsuarios = "SELECT * FROM usuarios";
+
+    $resultadoTodosUsuarios = $conn->query($sqlTodosUsuarios);
+
+    while($linha = $resultadoTodosUsuarios->fetch_assoc()){
+
+
+        echo "  <tr>
+                    <td>". $linha['id'] . "</td>
+                    <td>". $linha['usuario'] . "</td>
+                    <td>". $linha['senha'] . "</td>
+                </tr>
+        ";
+
+    }           //serve para mostrar os dados de cada usuário na tabela HTML
+    
+    ?>
+
+    
+
+
+</table>
